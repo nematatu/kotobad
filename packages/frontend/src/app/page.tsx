15bf1fb9a466ca28b31e4ee9dbfa66108cca17dc
nematatu <nematatu1003@gmail.com>
2025-11-26 10:51:00 +0900

    feat: lp初期デザイン

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,13 +1,24 @@
-import Link from "next/link";
+import Logo from "@/assets/logo/logo.svg";
+import LogoMoji from "@/assets/logo/logo-moji.svg";
 
 export default function Page() {
 	return (
-		<div className="flex flex-col gap-y-4">
-			<Link href="/threads">
-				<div className="text-blue-400 underline font-bold text-3xl">
-					スレッド一覧
+		<>
+			<div className="flex mx-auto justify-center h-[80vh]">
+				<div className="mt-20 flex items-center flex-col gap-y-8">
+					<div className="flex items-center gap-x-8">
+						<Logo className="w-25" />
+						<LogoMoji className="w-50" />
+					</div>
+					<div className="justify-end text-text-emphasis text-2xl font-bold">
+						<h1>
+							<span>バド</span>
+							<span>好きのための</span>
+							<span>掲示板</span>
+						</h1>
+					</div>
 				</div>
-			</Link>
-		</div>
+			</div>
+		</>
 	);
 }
