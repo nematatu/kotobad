d21f3a29d9e8a18c97d36ba07eba98eaffc5a3f9
nematatu <nematatu1003@gmail.com>
2025-12-20 13:26:12 +0900

    feat: ホームでスレッドをグリッド表示

---
+++ packages/frontend/src/app/components/view/threads/threadList.tsx
@@ -0,0 +1,57 @@
+import type { LabelType } from "@kotobad/shared/src/types";
+import type { ThreadType } from "@kotobad/shared/src/types/thread";
+import Link from "next/link";
+import ChatIcon from "@/assets/threads/chat.svg";
+import { formatDate } from "@/utils/date/formatDate";
+import { getRelativeDate } from "@/utils/date/getRelativeDate";
+
+type ThreadListType = {
+	threads: ThreadType[];
+};
+
+export const ThreadList = ({ threads }: ThreadListType) => {
+	const threadList: ThreadType[] = threads;
+
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+			{threadList.map((thread) => (
+				<Link
+					href={`/threads/${thread.id}`}
+					key={thread.id}
+					className={`group dark:text-gray-300 p-4 min-h-14 flex items-center border cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 `}
+				>
+					<div className="flex-col flex sm:flex-row justify-between text-sm sm:text-base w-full">
+						{/* 左側：タイトル + 投稿日時 */}
+						<div className="min-w-0 flex-1 pr-4">
+							<div className="flex space-x-4">
+								<span className="font-bold text-lg sm:text-xl group-hover:text-blue-500 block  text-ellipsis line-clamp-2 sm:line-clamp-none sm:whitespace-normal break-words">
+									{thread.title}
+								</span>
+							</div>
+							<div className="flex items-center space-x-1 text-gray-500">
+								<span className="text-sm">
+									{getRelativeDate(thread.createdAt)}
+								</span>
+								<ChatIcon width={1} />
+								<div className="text-sm">{thread.postCount}</div>
+							</div>
+
+							{thread.threadLabels?.map(
+								(tl: LabelType.ThreadThreadLabelType) => (
+									<span key={tl.labelId} className="">
+										{tl.labels.name}
+									</span>
+								),
+							)}
+						</div>
+						{/* 右側：作成者 + 日付 */}
+						<div className="flex flex-col items-end gap-y-1 text-gray-500 text-xs sm:text-sm whitespace-nowrap">
+							<span>{thread.author.username}</span>
+							<span>{formatDate(thread.createdAt)}</span>
+						</div>
+					</div>
+				</Link>
+			))}
+		</div>
+	);
+};
--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
-import { ThreadList } from "./threads/components/view/ThreadList";
+import { ThreadList } from "./components/view/threads/threadList";
 import { getThreads } from "./threads/lib/getThread";
 
 export default async function Page() {
 	const { threads } = await getThreads(1);
 	return (
 		<div className="flex flex-col gap-y-4 mx-40">
-			<h1>人気のスレッド</h1>
-			<h1>最新のスレッド</h1>
+			<h1 className="text-4xl font-bold">人気のスレッド</h1>
+			<h1 className="text-4xl font-bold">最新のスレッド</h1>
 			<ThreadList threads={threads} />
 		</div>
 	);
