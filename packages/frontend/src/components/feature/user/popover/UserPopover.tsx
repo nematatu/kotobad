38d997f5dae564782e1195610053f9b706f59a21
nematatu <nematatu1003@gmail.com>
2025-12-22 13:32:24 +0900

    fix: deploy.yml

--- packages/frontend/src/components/feature/user/popover/UserPopover.tsx
+++ packages/frontend/src/components/feature/user/popover/UserPopover.tsx
@@ -28,28 +28,6 @@ export function UserPopover() {
 					</Link>
 				</div>
 				<IconButton icon={<ThumbsUp />}>Like</IconButton>
-				<div className="grid gap-2 p-4">
-					<div className="grid grid-cols-3 items-center gap-4">
-						<Label htmlFor="maxWidth">Max. width</Label>
-						<Input
-							id="maxWidth"
-							defaultValue="300px"
-							className="col-span-2 h-8"
-						/>
-					</div>
-					<div className="grid grid-cols-3 items-center gap-4">
-						<Label htmlFor="height">Height</Label>
-						<Input id="height" defaultValue="25px" className="col-span-2 h-8" />
-					</div>
-					<div className="grid grid-cols-3 items-center gap-4">
-						<Label htmlFor="maxHeight">Max. height</Label>
-						<Input
-							id="maxHeight"
-							defaultValue="none"
-							className="col-span-2 h-8"
-						/>
-					</div>
-				</div>
 			</PopoverContent>
 		</Popover>
 	);
