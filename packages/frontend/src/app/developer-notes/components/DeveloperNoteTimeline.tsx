433cc61b2cd2441b77c632c5f4eac387cc44dfd6
nematatu <nematatu1003@gmail.com>
2026-03-14 01:43:26 +0900

    style: ボヤキに投稿時間表示

--- packages/frontend/src/app/developer-notes/components/DeveloperNoteTimeline.tsx
+++ packages/frontend/src/app/developer-notes/components/DeveloperNoteTimeline.tsx
@@ -45,6 +45,9 @@ const formatDottedDate = (value: string) => {
 		year: "numeric",
 		month: "2-digit",
 		day: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: false,
 	});
 	const date = new Date(value);
 
